fca5e2a9a383ea0f5f81ca9da8979dd56912e9ac
DELL <DELL@DESKTOP-IS8M5MD>
2022-03-03 17:42:45 +0530

    First Commit

---
+++ Project1/src/com/codebode/project/HelloWorld.java
@@ -0,0 +1,11 @@
+package com.codebode.project;
+
+public class HelloWorld {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		System.out.println("Hi");
+
+	}
+
+}
